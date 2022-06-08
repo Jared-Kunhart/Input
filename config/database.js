38fee176692bedfd55c722f463a0134e58a94ff6
Jared Kunhart <hakujaden@comcast.net>
2022-06-07 21:05:00 -0500

    test

--- config/database.js
+++ config/database.js
@@ -9,6 +9,11 @@ module.exports = {
       database,
       host,
       dialect: 'postgres',
+      native: true,
+      ssl: true,
+      dialectOptions: {
+        ssl: true
+      },
     },
     production: {
         use_env_variable: 'DATABASE_URL',
